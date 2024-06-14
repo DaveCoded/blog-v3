127d8fccbd73836f89246380aa6f48f5f2c2e818
Dave Bernhard <davidbernhard@live.com>
2024-06-14 12:31:27 +0300

    comment outlining approach

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,21 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+    /* Selective publishing:
+     *
+     * Decide what to base this on: drafts? Publish flag? On or off by default? Configurable?
+     * Add to collection(s): maybe all collections can extend a base one that includes this?
+     * Filter posts (and other types) accordingly for items
+     * Make sure links are correct
+     */
+    const posts = await getCollection('blog');
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site,
+        items: posts.map((post) => ({
+            ...post.data,
+            link: `/blog/${post.slug}/`,
+        })),
+    });
 }
