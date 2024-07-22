235cb03f43a9645b9d3f43ea5985611a1c530b20
Dave Bernhard <davidbernhard@live.com>
2024-07-22 19:25:11 -0400

    use nord theme for syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://davebernhard.com',
     integrations: [mdx(), sitemap()],
+
+    markdown: {
+        shikiConfig: {
+            theme: 'nord',
+        },
+    },
 });
